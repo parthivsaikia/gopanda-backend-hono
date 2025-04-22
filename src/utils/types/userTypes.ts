a7a86e301f108e9501109b195f8e12306db6b25a
parthivsaikia <parthivsaikia985@gmail.com>
2025-04-22 20:35:16 +0530

    add user DTO

---
+++ src/utils/types/userTypes.ts
@@ -0,0 +1,34 @@
+import { UserRole } from "../../../prisma/generated/prisma/index.js";
+import { type } from "arktype";
+
+//UserDTOs
+
+//password validation:
+const Password = type("string").narrow((password, ctx) => {
+  if (password.length < 8) {
+    return ctx.reject("Length of password must be greater than 8");
+  }
+  if (!/[A-Z]/.test(password)) {
+    return ctx.reject("Password must contain an uppercase letter.");
+  }
+  if (!/[a-z]/.test(password)) {
+    return ctx.reject("Password must contain a lowercase letter.");
+  }
+  if (!/\d/.test(password)) {
+    return ctx.reject("Password must contain a digit.");
+  }
+  return true;
+});
+
+export const UserInputUserDTO = type({
+  name: "string > 3",
+  username: "string > 5",
+  password: Password,
+  email: "string.email",
+  state: "string",
+  country: "string",
+  mobileNumber: "string.numeric",
+  role: type.valueOf(UserRole),
+});
+
+export type UserInputUserDTO = typeof UserInputUserDTO.infer;
