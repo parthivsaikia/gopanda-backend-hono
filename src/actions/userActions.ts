c231a3ad20976c457bc3deb14aa5dce93d72b2f8
parthivsaikia <parthivsaikia985@gmail.com>
2025-04-22 12:31:23 +0530

    add getUser action

--- src/actions/userActions.ts
+++ src/actions/userActions.ts
@@ -4,6 +4,7 @@ export const createUser = async (data: UserInputUserDTO) => {
   try {
     const user = await prisma.user.create({
       data,
+      omit: { password: true },
     });
     return user;
   } catch (error) {
@@ -11,6 +12,25 @@ export const createUser = async (data: UserInputUserDTO) => {
       error instanceof Error
         ? `Error in creating user: ${error.message}`
         : `Unknown error in creating user.`;
+    throw new Error(errorMessage);
+  }
+};
+
+export const getUser = async (username: string) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { username },
+      omit: { password: true },
+    });
+    if (!user) {
+      throw new Error(`User with username ${username} not found.`);
+    }
+    return user;
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error
+        ? `Error in finding user ${username} : ${error.message}`
+        : `Unknown error in finding user ${username}`;
     throw new Error(errorMessage);
   }
 };
