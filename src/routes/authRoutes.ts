120811381ea86946a5819ee1f5b9452739130921
parthivsaikia <parthivsaikia985@gmail.com>
2025-04-23 00:14:02 +0530

    add logout routes to authRouter

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,7 +1,17 @@
 import { Hono } from "hono";
-import { login, signup } from "~/controllers/authControllers";
+import {
+  login,
+  logout,
+  logoutAll,
+  signup,
+} from "~/controllers/authControllers";
+import { authMiddleware } from "~/utils/middlewares";
 
 const authRouter = new Hono();
 
 authRouter.post("/signup", signup);
 authRouter.post("/login", login);
+authRouter.post("/logout", authMiddleware, logout);
+authRouter.post("/logout-all", authMiddleware, logoutAll);
+
+export default authRouter;
