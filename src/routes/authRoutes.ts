b64f23bf14e5efe63dbd2f35f3c8973df66e0e22
parthivsaikia <parthivsaikia985@gmail.com>
2025-04-24 01:43:45 +0530

    add auth middlewares to the routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -5,13 +5,13 @@ import {
   logoutAll,
   signup,
 } from "~/controllers/authControllers";
-import { authMiddleware } from "~/utils/middlewares";
+import { authMiddleware, csrfMiddleware } from "~/utils/middlewares";
 
 const authRouter = new Hono();
 
 authRouter.post("/signup", signup);
 authRouter.post("/login", login);
-authRouter.post("/logout", authMiddleware, logout);
-authRouter.post("/logout-all", authMiddleware, logoutAll);
+authRouter.post("/logout", authMiddleware, csrfMiddleware, logout);
+authRouter.post("/logout-all", authMiddleware, csrfMiddleware, logoutAll);
 
 export default authRouter;
